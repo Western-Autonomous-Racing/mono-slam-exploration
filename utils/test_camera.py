b84c25a8400f422117d0fc95a0160d95af597fa0
d4n13lx13 <danxie2001@gmail.com>
2023-11-26 23:15:04 -0500

    working opencv example for b0179

--- utils/test_camera.py
+++ utils/test_camera.py
@@ -1,14 +1,31 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+# GStreamer pipeline for Arducam B0179
+pipeline = (
+    "nvarguscamerasrc ! video/x-raw(memory:NVMM),width=1920,height=1080,format=(string)NV12,framerate=(fraction)30/1 ! "
+    "nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! appsink"
+)
+# Create a VideoCapture object with the GStreamer pipeline
+cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
+# Check if the camera is opened successfully
+if not cap.isOpened():
+    print("Failed to open camera")
+    exit()
+
+# Read and display frames from the camera
 while True:
     ret, frame = cap.read()
 
-    cv2.imshow('frame', frame)
+    if not ret:
+        print("Failed to read frame from camera")
+        break
+    
+    cv2.imshow("Camera Stream", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Release the camera and close windows
 cap.release()
 cv2.destroyAllWindows()
