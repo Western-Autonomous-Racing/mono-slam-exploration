f1e43d784a9f722f3618f3ffb1c6b88a1163f733
d4n13lx13 <danxie2001@gmail.com>
2023-11-28 23:06:09 -0500

    tested pipeline

--- utils/test_camera.py
+++ utils/test_camera.py
@@ -1,10 +1,18 @@
 import cv2
 
 # GStreamer pipeline for Arducam B0179
+# grayscale
+# pipeline = (
+#     "nvarguscamerasrc ! video/x-raw(memory:NVMM),width=1920,height=1080,format=(string)NV12,framerate=(fraction)30/1 ! "
+#     "nvvidconv ! videoconvert ! videoflip method=rotate-180 ! appsink"
+# )
+
+# RGB
 pipeline = (
     "nvarguscamerasrc ! video/x-raw(memory:NVMM),width=1920,height=1080,format=(string)NV12,framerate=(fraction)30/1 ! "
-    "nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! appsink"
+    "nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! videoflip method=rotate-180  ! appsink"
 )
+
 # Create a VideoCapture object with the GStreamer pipeline
 cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
